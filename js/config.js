2b49c2aa2680a547ae77e6c21f105fb4a8d40e68
Axe-08 <23ucs529@lnmiit.ac.in>
2025-02-09 08:54:53 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 const config = {
-    API_URL: 'https://secret-participation-flour-briefs.trycloudflare.com',
+    API_URL: 'https://payment-spectrum-permission-follow.trycloudflare.com',
     SOCKET_OPTIONS: {
         transports: ['websocket'],
         reconnection: true,

70c3a6c6644067ceebe8867dc1a3a27802a7e7d3
Axe-08 <23ucs529@lnmiit.ac.in>
2025-02-09 11:06:47 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 const config = {
-    API_URL: 'https://floors-environmental-budget-nurse.trycloudflare.com',
+    API_URL: 'https://sheet-stamps-products-website.trycloudflare.com',
     SOCKET_OPTIONS: {
         transports: ['websocket'],
         reconnection: true,
